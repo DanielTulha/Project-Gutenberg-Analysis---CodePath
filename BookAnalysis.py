2bfacd0af64685c3766bfa7e030a8076c81b446b
Tulha_Purdue <hochst10@purdue.edu>
2020-07-18 21:11:33 -0400

    BookAnalysis Class
    Simpllfied functions as they can now share the same word list and avoid repeated parsing of text file

--- BookAnalysis.py
+++ BookAnalysis.py
@@ -5,202 +5,194 @@ import random
 bookName = "A Tale Of Two Cities - Charles Dickens.txt"
 mostCommonWordsFile = "1000mostCommonWords.txt"
 
-def getWordList(fileName):
-    file = open(fileName, 'rt', encoding='utf-8')
-    text = file.read()
-    file.close()
+class BookAnalysis():
+    def __init__(self, bookName):
+        self.bookName = bookName
+        self.wordList = self.getWordList(bookName)
 
-    words = text.split()
-    return words
+    def getWordList(self, fileName):
+        file = open(fileName, 'rt', encoding='utf-8')
+        text = file.read()
+        file.close()
 
-def getTotalNumberOfWords(fileName):
+        words = text.split()
+        return words
 
-    wordList = getWordList(fileName)
-    return len(wordList)
+    def getTotalNumberOfWords(self):
+        return len(self.wordList)
 
-def getTotalUniqueWords(fileName):
+    def getTotalUniqueWords(self):
+        uniqueWords = set(self.wordList)
+        return len(uniqueWords)
 
-    words = getWordList(fileName)
-    uniqueWords = set(words)
-    return len(uniqueWords)
+    def getMostCommonWordsInEnglish(self, numberOfWords):
+        commonWordList = self.getWordList(mostCommonWordsFile)
+        return commonWordList[:numberOfWords]
 
-def getMostCommonWordsInEnglish(numberOfWords):
-    commonWordList = getWordList(mostCommonWordsFile)
-    return commonWordList[:numberOfWords]
+    def getMostFrequentWords(self, numberOfWords, wordFilter = set()):
 
-def getMostFrequentWords(fileName, numberOfWords, wordFilter = set()):
+        wordCount = collections.defaultdict(int)
 
-    wordCount = collections.defaultdict(int)
+        class Word:
+            def __init__(self, word):
+                self.word = word
 
-    class Word:
-        def __init__(self, word):
-            self.word = word
+            def getWord(self):
+                return self.word
 
-        def getWord(self):
-            return self.word
+            def __lt__(self, other):
+                return wordCount[self.word] < wordCount[other.getWord()]
 
-        def __lt__(self, other):
-            return wordCount[self.word] < wordCount[other.getWord()]
+        mostCommonWords = []
+        wordSet = set()
 
-    wordList = getWordList(fileName)
+        for word in self.wordList:
+            if word in wordFilter or word.lower() in wordFilter:
+                continue
 
-    mostCommonWords = []
-    wordSet = set()
+            wordCount[word] += 1
 
-    for word in wordList:
-        if word in wordFilter or word.lower() in wordFilter:
-            continue
+            if word in wordSet:
+                heapify(mostCommonWords)
+                continue
 
-        wordCount[word] += 1
+            if len(mostCommonWords) < numberOfWords:
+                newWord = Word(word)
+                heappush(mostCommonWords, newWord)
+                wordSet.add(word)
 
-        if word in wordSet:
-            heapify(mostCommonWords)
-            continue
+            elif wordCount[word] > wordCount[mostCommonWords[0].getWord()]:
+                newWord = Word(word)
+                removed = heapreplace(mostCommonWords, newWord)
+                wordSet.remove(removed.getWord())
+                wordSet.add(word)
 
-        if len(mostCommonWords) < numberOfWords:
-            newWord = Word(word)
-            heappush(mostCommonWords, newWord)
-            wordSet.add(word)
+        frequentWordsDescending = collections.deque([])
+        while mostCommonWords:
+            w = heappop(mostCommonWords).getWord()
+            frequentWordsDescending.appendleft([w, wordCount[w]])
 
-        elif wordCount[word] > wordCount[mostCommonWords[0].getWord()]:
-            newWord = Word(word)
-            removed = heapreplace(mostCommonWords, newWord)
-            wordSet.remove(removed.getWord())
-            wordSet.add(word)
-
-    frequentWordsDescending = collections.deque([])
-    while mostCommonWords:
-        w = heappop(mostCommonWords).getWord()
-        frequentWordsDescending.appendleft([w, wordCount[w]])
-
-    return list(frequentWordsDescending)
-
-
-def getMostInterestingFrequentWords(fileName, numberOfWords, numberOfFilteredWords):
-    commonWordsSet = set(getMostCommonWordsInEnglish(numberOfFilteredWords))
-    return getMostFrequentWords(fileName, numberOfWords, commonWordsSet)
-
-def getLeastFrequentWords(fileName, numberOfWords):
-
-    wordCount = collections.defaultdict(int)
-
-    class Word:
-        def __init__(self, word):
-            self.word = word
-
-        def getWord(self):
-            return self.word
-
-        def __lt__(self, other): # Max heap comparator
-            return wordCount[self.word] > wordCount[other.getWord()]
-
-    wordList = getWordList(fileName)
-
-    leastCommonWords = []
-    wordSet = set()
-
-    for word in wordList:
-        wordCount[word] += 1
-
-        if word in wordSet:
-            heapify(leastCommonWords)
-            continue
-
-        if len(leastCommonWords) < numberOfWords:
-            newWord = Word(word)
-            heappush(leastCommonWords, newWord)
-            wordSet.add(word)
-
-        elif wordCount[word] < wordCount[leastCommonWords[0].getWord()]:
-            newWord = Word(word)
-            removed = heapreplace(leastCommonWords, newWord)
-            wordSet.remove(removed.getWord())
-            wordSet.add(word)
-
-    leastCommonWordsDescending = collections.deque([])
-    while leastCommonWords:
-        w = heappop(leastCommonWords).getWord()
-        leastCommonWordsDescending.appendleft([w, wordCount[w]])
-    return list(leastCommonWordsDescending)
+        return list(frequentWordsDescending)
 
 
-def getFrequencyOfWord(fileName, desiredWord):
-    wordList = getWordList(fileName)
+    def getMostInterestingFrequentWords(self, numberOfWords, numberOfFilteredWords):
+        commonWordsSet = set(self.getMostCommonWordsInEnglish(numberOfFilteredWords))
+        return self.getMostFrequentWords(numberOfWords, commonWordsSet)
 
-    currentChapter = 0
-    wordsPerChapter = []
+    def getLeastFrequentWords(self, numberOfWords):
 
-    for word in wordList:
-        if word == 'Chapter':
-            wordsPerChapter.append(0)
+        wordCount = collections.defaultdict(int)
 
-        if word == desiredWord and wordsPerChapter:
-            wordsPerChapter[-1] += 1
+        class Word:
+            def __init__(self, word):
+                self.word = word
 
-    return wordsPerChapter
+            def getWord(self):
+                return self.word
 
-def getChapterQuoteAppears(fileName, desiredQuote):
-    wordList = getWordList(fileName)
+            def __lt__(self, other): # Max heap comparator
+                return wordCount[self.word] > wordCount[other.getWord()]
 
-    currentChapter = matchedWords = wordIndex = 0
-    quote = desiredQuote.split()
+        leastCommonWords = []
+        wordSet = set()
 
-    while wordIndex < len(wordList):
-        word = wordList[wordIndex]
-        if word == 'Chapter':
-            currentChapter += 1
+        for word in self.wordList:
+            wordCount[word] += 1
 
-        if word == quote[matchedWords]:
-            matchedWords += 1
-            if matchedWords == len(quote):
-                return currentChapter
+            if word in wordSet:
+                heapify(leastCommonWords)
+                continue
 
-        elif matchedWords > 0:
-            wordIndex = wordIndex - matchedWords
-            matchedWords = 0
+            if len(leastCommonWords) < numberOfWords:
+                newWord = Word(word)
+                heappush(leastCommonWords, newWord)
+                wordSet.add(word)
 
-        wordIndex += 1
+            elif wordCount[word] < wordCount[leastCommonWords[0].getWord()]:
+                newWord = Word(word)
+                removed = heapreplace(leastCommonWords, newWord)
+                wordSet.remove(removed.getWord())
+                wordSet.add(word)
 
-    return -1
+        leastCommonWordsDescending = collections.deque([])
+        while leastCommonWords:
+            w = heappop(leastCommonWords).getWord()
+            leastCommonWordsDescending.appendleft([w, wordCount[w]])
+        return list(leastCommonWordsDescending)
+
+    def getFrequencyOfWord(self, desiredWord):
+
+        currentChapter = 0
+        wordsPerChapter = []
+
+        for word in self.wordList:
+            if word == 'Chapter':
+                wordsPerChapter.append(0)
+
+            if word == desiredWord and wordsPerChapter:
+                wordsPerChapter[-1] += 1
+
+        return wordsPerChapter
+
+    def getChapterQuoteAppears(self, desiredQuote):
+        currentChapter = matchedWords = wordIndex = 0
+        quote = desiredQuote.split()
+
+        while wordIndex < len(self.wordList):
+            word = self.wordList[wordIndex]
+            if word == 'Chapter':
+                currentChapter += 1
+
+            if word == quote[matchedWords]:
+                matchedWords += 1
+                if matchedWords == len(quote):
+                    return currentChapter
+
+            elif matchedWords > 0:
+                wordIndex = wordIndex - matchedWords
+                matchedWords = 0
+
+            wordIndex += 1
+
+        return -1
 
 
-def generateSentence(fileName, sentenceLength = 20):
+    def generateSentence(self, sentenceLength = 20):
 
-    wordList = getWordList(fileName)
+        def generateSentenceRecursively(currentWord, currentSentence):
+            currentSentence += ' ' + currentWord
+            if len(currentSentence.split()) == sentenceLength:
+                return currentSentence
 
-    def generateSentenceRecursively(currentWord, currentSentence):
-        currentSentence += ' ' + currentWord
-        if len(currentSentence.split()) == sentenceLength:
-            return currentSentence
+            wordIndexes = []
 
-        wordIndexes = []
+            for wordIndex in range(len(self.wordList)):
+                if currentWord == self.wordList[wordIndex]:
+                    wordIndexes.append(wordIndex + 1)
 
-        for wordIndex in range(len(wordList)):
-            if currentWord == wordList[wordIndex]:
-                wordIndexes.append(wordIndex + 1)
+            randomIndex = random.randrange(len(wordIndexes))
+            randomWord = self.wordList[wordIndexes[randomIndex]]
+            return generateSentenceRecursively(randomWord, currentSentence)
 
-        randomIndex = random.randrange(len(wordIndexes))
-        randomWord = wordList[wordIndexes[randomIndex]]
-        return generateSentenceRecursively(randomWord, currentSentence)
-
-    return generateSentenceRecursively('The', '')
+        return generateSentenceRecursively('The', '')
 
 
+# twoCities = BookAnalysis(bookName)
 # print()
-# print('A Tale Of Two Cities contains %s words' % getTotalNumberOfWords(bookName))
+# print('A Tale Of Two Cities contains %s words' % twoCities.getTotalNumberOfWords())
 # print()
-# print('A Tale Of Two Cities contains %s unique words' % getTotalUniqueWords(bookName))
+# print('A Tale Of Two Cities contains %s unique words' % twoCities.getTotalUniqueWords())
 # print()
-# print('The twenty most common words in A Tale Of Two Cities are', str(getMostFrequentWords(bookName, 20)))
+# print('The twenty most common words in A Tale Of Two Cities are', str(twoCities.getMostFrequentWords(20)))
 # print()
-# print('The twenty most interesting words in A Tale Of Two Cities are', str(getMostInterestingFrequentWords(bookName, 20, 1000)))
+# print('The twenty most interesting words in A Tale Of Two Cities are', str(twoCities.getMostInterestingFrequentWords(20, 1000)))
 # print()
-# print('The twenty least common words in A Tale Of Two Cities are', str(getLeastFrequentWords(bookName, 20)))
+# print('The twenty least common words in A Tale Of Two Cities are', str(twoCities.getLeastFrequentWords(20)))
 # print()
-# print("The frequency of 'Monsieur' by chapter is", str(getFrequencyOfWord(bookName, 'Monsieur')))
+# print("The frequency of 'Monsieur' by chapter is", str(twoCities.getFrequencyOfWord('Monsieur')))
 # print()
-# print("The quote 'The coffee-room had no other occupant,' can be found in chapter", getChapterQuoteAppears(bookName, 'The coffee-room had no other occupant,'))
+# print("The quote 'The coffee-room had no other occupant,' can be found in chapter", twoCities.getChapterQuoteAppears('The coffee-room had no other occupant,'))
 # print()
 # for i in range(20):
-#     print('Sample random sentence:', generateSentence(bookName))
+#     print('Sample random sentence:', twoCities.generateSentence())
 # print()
